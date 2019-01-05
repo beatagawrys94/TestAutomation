dac40a735e201036c3dcd4740a469e1ab9bc3d8b
beatagawrys94 <mariuszwlazly24>
2019-01-05 19:30:24 +0100

    added assertions

--- functions.py
+++ functions.py
@@ -5,3 +5,7 @@ def findSearchButton(driver):
 def findField(driver):
     pole = driver.find_element_by_xpath('//*[@id="id-search-field"]')
     return pole
+
+def search2(driver):
+    searchsecond=driver.find_element_by_xpath('/html/body/div/div[3]/div/section/form/p/input[1]')
+    return searchsecond
--- tests.py
+++ tests.py
@@ -10,15 +10,26 @@ class TC_1(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        driver = self.driver
+        driver.get("http://www.python.org")
+        time.sleep(2)
 
     def test_TC_1(self):
         driver = self.driver
-        driver.get("http://www.python.org")
+        #driver.get("http://www.python.org")
         lol=functions.findField(driver)
         lol.send_keys("lol")
+        teksts = lol.get_attribute('value')
+        assert "lol" in teksts
         go=functions.findSearchButton(driver)
         go.click()
-        time.sleep(3)
+        time.sleep(2)
+        sr2=functions.search2(driver)
+        sr2.clear()
+        sr2.send_keys("Python")
+        tekst=sr2.get_attribute('value')
+        assert "Python" in tekst
+        time.sleep(2)
 
     def tearDown(self):
         self.driver.close()
